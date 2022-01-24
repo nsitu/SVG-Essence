736899c52d715fc5621d9b7b7521a8387e4edc33
MeowShadow <harold.sikkema@sheridancollege.ca>
2022-01-24 09:55:39 -0500

    amking things work

--- index.js
+++ index.js
@@ -1,7 +1,114 @@
-const express = require('express')
+
+const fs = require('fs');
+
+
+const express = require('express');
+
+// express middleware for uploading files.
+const fileUpload = require('express-fileupload');
+
 const {spawn} = require('child_process');
+//const { exec } = require('child_process');
+
 const app = express()
 const PORT = process.env.PORT || 5000
+
+// serve up a static frontend.
+app.use(express.static('public'))
+
+app.use(fileUpload());
+
+
+// Thanks:  https://stackoverflow.com/a/50243907/17929842
+app.post('/upload', function(req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    } 
+
+    let svg = req.files.file;   
+
+    let dir = './uploads';
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    
+    let destination = `${dir}/${svg.md5}.svg`;
+    // Move the uploaded svg to the uploads folder.
+    svg.mv(destination, function(err) {
+        if (err) return res.status(500).send(err) 
+        // create a status report.
+        let status = {
+            "name": svg.name,
+            "filepath": destination,
+            "md5" :  svg.md5,
+            "datetime": new Date().toLocaleString(),
+            "status" : "File Uploaded",
+        }
+        // save the status to a sidecar JSON file
+        fs.promises
+            .writeFile('./uploads/'+svg.md5+'.json', JSON.stringify(status))
+            .then(() => { 
+                  try { 
+                    let output = [];
+
+                    let args = [
+                       '--verbose',
+                      'read', status.filepath,
+                      'scaleto', '23in', '23in',
+                      'linemerge', '--tolerance', '1in',
+                      'linesimplify', '--tolerance', '0.01in',
+                      'write', '--page-size', '24inx24in', '--center',
+                      status.filepath+'_output.svg'
+                  ]  
+
+                  // exec(command.join(' '), (error, stdout, stderr) => {
+                  //   if (error) {
+                  //     console.error(`exec error: ${error}`);
+                  //     return;
+                  //   }
+                  //   console.log(`stdout: ${stdout}`);
+                  //   console.error(`stderr: ${stderr}`);
+                  //   res.send(JSON.stringify(stdout))
+
+                  // });
+
+
+                    const vpype = spawn('vpype', args );
+
+                    vpype.on('error', (err) => {
+                      console.error('Failed to start subprocess.', err);
+                    });
+
+                    //if there is any output, record it
+                    vpype.stdout.on('data', (data) =>  {
+                      console.log(data.toString('utf8'))
+                      output.push(data.toString('utf8')) 
+                    })
+
+                    vpype.stderr.on('data', (data) =>  {
+                      console.log(data.toString('utf8'))
+                      output.push(data.toString('utf8')) 
+                    })
+                    
+                    vpype.on('close', (code, signal) => {
+                      console.log('vpype '+args.join(' '));
+                        console.log(output);
+                        console.log(code);
+                        console.log(signal);
+                        console.log(`Child process closed all stdio with code ${code}`);
+                        
+                        // res.send(status) 
+                        res.send(JSON.stringify(output))
+                    });
+                  } 
+                  catch(err) {
+                      console.error(err) 
+                  }
+                }
+            )
+            .catch(err =>  console.error(status, err) ) 
+
+    });
+  });
+ 
 
 app.get('/', (req, res) => {
 var largeDataSet = [];
---
+++ public/index.js
@@ -0,0 +1,50 @@
+
+let uploadedSVG = '';
+ 
+const svgContainer = document.getElementById('svgContainer'); 
+const dragAndDrop = document.getElementById('dragAndDrop'); 
+
+// Check if browser supports file drag and drop
+if (typeof window.FileReader === 'undefined') {
+    // notify users that browser does not support file drag and drop 
+    dragAndDrop.innerHTML = '<p>Sorry, drag and drop is not working.</p>';
+} else {
+    // provide instructions for drag and drop. 
+    dragAndDrop.innerHTML = '<p>Drag and drop an .svg file here.</p>';
+}
+
+// hover styling for drag and drop
+dragAndDrop.ondragover = function() {
+    this.className = 'hover';
+    return false;
+}
+dragAndDrop.ondragend = function() {
+    this.className = '';
+    return false;
+}
+
+dragAndDrop.ondrop = function(e) { 
+    this.className = '' 
+    e.preventDefault()
+    // https://developer.mozilla.org/en-US/docs/Web/API/FileReader 
+    // FileReader asynchronously reads files (or raw data buffers) using File or Blob objects
+    let file = e.dataTransfer.files[0]
+
+    // mime types are lowercase https://w3c.github.io/FileAPI/#dfn-type
+    if (file.type != "image/svg+xml"){
+        alert("Sorry, you can't upload that. It is not an SVG file.");
+    }
+    else{
+        let formData = new FormData()
+        formData.append('file', file) 
+        fetch('/upload', {  method: 'POST',  body: formData  })
+            .then(response => response.json())
+            .then((data) => { console.log('finished upload', data); })
+            .catch((err) => { console.log('upload error', err); })
+    } 
+    return false;
+};
+ 
+ 
+    
+  
