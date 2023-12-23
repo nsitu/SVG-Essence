a449a2b764940705a1d24ad9c3b573a61e101227
Harold Sikkema <harold@nsitu.ca>
2023-12-23 11:37:05 -0400

    uploads

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,8 @@ FROM nikolaik/python-nodejs:python3.12-nodejs20-alpine
 # All images have a default user pn with uid 1000 and gid 1000. 
 WORKDIR /home/pn/app 
 COPY . /home/pn/app  
-
 # install dependencies with Alpine Package Keeper  
 RUN apk add --no-cache geos 
-
-
 RUN pip install --no-cache-dir -r requirements.txt 
 RUN npm install --production && npm cache clean --force 
 ENV NODE_ENV production
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {spawn} = require('child_process');
 //const { exec } = require('child_process');
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 // serve up a static frontend.
 app.use(express.static('public'))
@@ -20,6 +20,7 @@ app.use(fileUpload());
 
 // folder to store uploads
 const dir = __dirname+'/uploads';
+console.log('lets confirm existence of ' + dir)
 if (!fs.existsSync(dir)) fs.mkdirSync(dir); 
 
 
